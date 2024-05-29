cedab46c4226c05f0cc647ea43ea98b2d60d7e0f
Renan Estery <renan.estery@compasso.com.br>
2024-05-29 11:37:05 -0300

    feat: config routes publics

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/Signin';
-import { Signup } from '@screens/Signup';
+
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Signup /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
