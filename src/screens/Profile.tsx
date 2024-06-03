a099a98a9ba8880bc7f96de51dc1cca05789396d
Renan Estery <renan.estery@compasso.com.br>
2024-06-03 16:01:01 -0300

    feat: add config gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { FileInfo } from 'expo-file-system';
 import {
   Center,
   ScrollView,
@@ -7,7 +10,7 @@ import {
   Skeleton,
   Text,
   Heading,
-  useTheme,
+  useToast,
 } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -19,6 +22,41 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/RenanEstery20.png'
+  );
+  const toast = useToast();
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+      if (photoSelected.canceled) {
+        return;
+      }
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = (await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        )) as FileInfo;
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 3) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -36,13 +74,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: 'https://github.com/RenanEstery20.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
